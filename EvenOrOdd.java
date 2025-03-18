ef530cf8f21df36e3ecc4f60f5798e0aa690c94f
Ritika <riyachhikara26@gmail.com>
2025-03-18 23:09:20 +0530

    added changel name

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class EvenOrOdd
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int n=sc.nextInt();
+		String result=(n%2==0)?n+" is Even" : n+" is Odd";
+		System.out.println(result);
+	}
+}
---
+++ ThreeDigitOrNot.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ThreeDigitOrNot
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		int n=sc.nextInt();
+		String result=(n>=100 && n<=999)?n+" is a 3-digit Number" : n+" is NOT a 3-digit Number";
+		System.out.println(result);
+	}
+}
